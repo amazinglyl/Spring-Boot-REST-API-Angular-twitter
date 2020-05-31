273d26bf87a5ba7f16a486e6d55c2204727bb0a4
lyl <ylluan@iastate.edu>
2020-05-31 16:34:00 -0500

    update

--- twitter/src/main/java/rest/twitter/CrosConfigurer.java
+++ twitter/src/main/java/rest/twitter/CrosConfigurer.java
@@ -1,10 +1,10 @@
 package rest.twitter;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.CorsRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class CrosConfigurer {
